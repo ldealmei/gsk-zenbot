f752b8294fdd0a98be09e4c9bf33bc198f084e2a
Luis Akakpo <luis.akakpo@gmail.com>
2018-01-18 06:06:48 +0100

    start prioritize tasks

--- Task.py
+++ Task.py
@@ -3,12 +3,13 @@ import datetime
 
 class Task:
 
-    def __init__(self, project_id, deadline, creator, dur_estim, priority, effort, description= {}):
+    def __init__(self, project_id, deadline, creator, dur_estim, importance, effort, description= {}):
         self.task_id = uuid.uuid4()
         self.progress = 0.0
         self.time_on_task = datetime.timedelta(0)
-        self.actors = [] #list of bot_ids
-        self.dependent_on = [] # list of Task objects
+        self.dependent_on = [] # list of Task_id
+        self.dependency_of = [] # list of Task_id
+
         self.date_completed = None
 
         self.project_id = project_id
@@ -16,8 +17,8 @@ class Task:
         self.deadline = deadline  # datetime object
         self.created_by = creator # bot_id
         self.dur_estim = dur_estim #timedelta object
-        self.priority = priority
-        self.effort = effort
+        self.importance = importance  # on a scale 1-3
+        self.effort = effort  # on a scale 1-5
 
     def assign_to_project(self):
         #TODO
