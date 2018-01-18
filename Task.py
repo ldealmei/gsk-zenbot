349eb61fc3b05a1b9787aa98c1e8f37683dfaddc
Luis Akakpo <luis.akakpo@gmail.com>
2018-01-18 07:04:35 +0100

    add categories to Task

--- Task.py
+++ Task.py
@@ -3,7 +3,7 @@ import datetime
 
 class Task:
 
-    def __init__(self, project_id, deadline, creator, dur_estim, importance, effort, description= {}):
+    def __init__(self, project_id, deadline, creator, dur_estim, importance, effort, category,  description= {}):
         self.task_id = uuid.uuid4()
         self.progress = 0.0
         self.time_on_task = datetime.timedelta(0)
@@ -19,6 +19,7 @@ class Task:
         self.dur_estim = dur_estim #timedelta object
         self.importance = importance  # on a scale 1-3
         self.effort = effort  # on a scale 1-5
+        self.category = category
 
     def assign_to_project(self):
         #TODO
