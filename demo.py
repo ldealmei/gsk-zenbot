668a0d4da3d11f1ee3e04e4eda3e9fb21c952bc2
Luis Akakpo <luis.akakpo@gmail.com>
2018-01-18 11:22:42 +0100

    demo improved

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ import datetime
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, Text
+from bokeh.models import ColumnDataSource, Text, HoverTool
 
 def get_random_cert() :
     cert = ''
@@ -23,7 +23,7 @@ def get_random_cert() :
 
 def task_generator():
     project_id = uuid.uuid4()
-    deadline  = datetime.date.today() + datetime.timedelta(days = randint(1,100))
+    deadline  = datetime.date.today() + datetime.timedelta(days = randint(1,21))
     creator = ''
     dur_estim = datetime.timedelta(hours=randint(1,4))
     importance = randint(1,4)
@@ -39,7 +39,7 @@ def event_generator():
     event_type = choice(['training', 'meeting'],1, p =[0.3, 0.7])
 
     if event_type == 'training' :
-        start = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days = randint(0,100)),datetime.time(hour=8))
+        start = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days = randint(0,21)),datetime.time(hour=8))
         duration = datetime.timedelta(hours=3)
         end= start + duration
         loc = {}
@@ -48,7 +48,7 @@ def event_generator():
         return Training(start, end, loc, certification )
 
     elif event_type == 'meeting' :
-        start = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days = randint(0,100)),datetime.time(hour=randint(8,14)))
+        start = datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days = randint(0,21)),datetime.time(hour=randint(8,14)))
         duration = datetime.timedelta(hours=randint(1,3))
         end = start + duration
 
@@ -73,7 +73,7 @@ def plot_events(events) :
             'name' : [str(ev.project_id) if isinstance(ev,Meeting)  else ev.certification for ev in events]}
 
     source = ColumnDataSource(data)
-    p = figure(plot_width = 1000, plot_height = 600)
+    p = figure(plot_width = 1000, plot_height = 600, x_range = [0,7], tools = ['xwheel_pan'])
 
     p.quad(top='top', bottom='bottom', left='left',
        right='right',
@@ -84,13 +84,44 @@ def plot_events(events) :
     # p.add_glyph(source, Text(x = 'bottom', y='left', text_color = 'black'))
     p.toolbar.logo = None
 
+    hover = HoverTool(tooltips = [('Name','@name')])
+
+    p.add_tools(hover)
+
     show(p)
+
+def event_overlap(ev1,ev2) :
+    overlap = False
+
+    set1 = set(range(ev1.start.hour, ev1.end.hour))
+    set2 = set(range(ev2.start.hour, ev2.end.hour))
+
+    for s in set1:
+        if s in set2:
+            overlap = True
+            break
+    print(overlap)
+    return overlap
+
+def resolve_conflicts(events) :
+        drop_events = []
+        for ev in events :
+            for ev_comp in events :
+                if ev.start.date() == ev_comp.start.date() :
+                    if ev != ev_comp :
+                        if event_overlap(ev,ev_comp):
+                            drop_events.append(ev_comp)
+
+        return [ev for ev in events if ev not in drop_events]
 
 
 params={'id':'',
-        'events' : [event_generator() for i in np.arange(100)],
-        'tasks' : [task_generator() for i in np.arange(50)]}
+        'events' : resolve_conflicts([event_generator() for i in np.arange(50)]),
+        'tasks' : [task_generator() for i in np.arange(100)]}
 
+# print(params['tasks'])
+print(len(params['events']))
 plot_events(params['events'])
 
-bot1 = Zenbot(params)
+test_bot = Zenbot(params)
+print(test_bot.make_today_schedule())
